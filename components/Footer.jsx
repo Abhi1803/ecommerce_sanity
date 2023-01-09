98b0fc18c75305ddafc11fa0929e5464c9e3e452
Abhinav Srivatssan <92371009+Abhi1803@users.noreply.github.com>
2023-01-09 17:15:47 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <p>2022 JSM Headphones All rights reserverd</p>
+       <p>Made with ❤ by Abhinav</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
